b5f1af95836c64aa8c467e09694e344245394931
Ozan Çağlayan <ozancag@gmail.com>
2016-05-06 12:45:40 +0300

    update

--- setup/02-users.sh
+++ setup/02-users.sh
@@ -3,15 +3,31 @@
 # Add lecturers group
 addgroup lecturers
 
+# Add lecturer users
+while IFS=, read USER PW; do
+    echo "Creating lecturer $USER"
+    if [ -z $PW ]; then
+        useradd -s "/bin/bash" -m -N -g users -G sudo,adm,lecturers $user
+    else
+        useradd -s "/bin/bash" -m -N -g users -G sudo,adm,lecturers -p "$PW" $user
+    fi
+done < <(egrep -v '^#' lecturers.list)
+
 # Add some admin users, add them to SSHD allowed list
-ADMINS="admin1 admin2 admin3"
+ADMINS=`tr -d "\n" "" < admins.list`
+echo "Administrators with SSH access: $ADMINS"
 echo "AllowUsers $ADMINS" >> /etc/ssh/sshd_config
 systemctl reload ssh.service
 
-for user in $ADMINS; do
-  # NOTE: You may need to provide them some default passwords
-  useradd -s "/bin/bash" -m -N -g users -G sudo,adm,lecturers $user
-done
+# Add regular users
+while IFS=, read USER PW; do
+    echo "Creating student $USER"
+    if [ -z $PW ]; then
+        useradd -s "/bin/bash" -m -N -g users $user
+    else
+        useradd -s "/bin/bash" -m -N -g users -p "$PW" $user
+    fi
+done < <(egrep -v '^#' users.list)
 
 # Create fontconfig-cache
 for u in $(ls /home/); do
